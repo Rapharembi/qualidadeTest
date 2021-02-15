a2bf5ab1a506153c6e5adb693695f03ca65c9ed1
Rapharembi <rapharembi2@gmail.com>
2021-02-15 19:12:28 -0300

    Add files via upload
    commitando alterações no teste do carrinho

--- intro_junit-master/test/carrinho/CarrinhoTest.java
+++ intro_junit-master/test/carrinho/CarrinhoTest.java
@@ -24,18 +24,41 @@ class CarrinhoTest {
 	
 	Carrinho carrinho;
 	Produto livro;
+	Produto livro2;
 	
 	@BeforeEach
 	public void inicializa() {
 		carrinho = new Carrinho();
 		livro = new Produto("Introdução ao Teste de Software", 100.00);
+		livro2 = new Produto("Introdução à Qualidade de Software", 150.00);
 	}
 	
 	@DisplayName("Testa adição ao carrinho")
 	@Test
 	public void testAdicao() {
-		
+		carrinho.addItem(livro);
+		int add = carrinho.getQtdeItems();
+		Assertions.assertEquals(1,add);
 	}
+	
+	@DisplayName("Testa o carrinho vazio")
+	@Test
+	public void testEsvaziar() {
+		carrinho.esvazia();
+		int vaz = carrinho.getQtdeItems();
+		Assertions.assertEquals(0,vaz);
+	}
+	
+	@DisplayName("Testa valor total do carrinho")
+	@Test
+	public void testValorTotal() {
+		carrinho.addItem(livro);
+		carrinho.addItem(livro2);
+		double tot = carrinho.getValorTotal();
+		Assertions.assertEquals(250.00,tot);
+	}
+	
+	
 	
 
 }
